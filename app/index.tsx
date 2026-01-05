3d7d14680f9c8e675d5cd547aa8c543ac917af5d
Jhonatan-calle <Jhonatancallegaleano@gmail.com>
2026-01-05 00:09:42 -0300

    firs version of index

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,50 @@
-import { useState, useEffect } from 'react'
-import { supabase } from './lib/supabase'
-import Auth from './components/Auth'
-import { View } from 'react-native'
-import { Session } from '@supabase/supabase-js'
+import { useEffect } from "react";
+import { View, ActivityIndicator, StyleSheet } from "react-native";
+import { router } from "expo-router";
+import { supabase } from "../utils/supabase";
 
-export default function App() {
-  const [session, setSession] = useState<Session | null>(null)
-
+export default function Index() {
   useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session)
-    })
+    checkSession();
+  }, []);
 
-    supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session)
-    })
-  }, [])
-  // mostrar login o iniciar secions
+  const checkSession = async () => {
+    // const { data: { session } } = await supabase.auth.getSession();
+    //
+    // if (!session) {
+    //   router.replace("/(auth)/login");
+    //   return;
+    // }
+    //
+    // const { data: profile } = await supabase
+    //   . from("profiles")
+    //   .select("role")
+    //   .eq("id", session.user.id)
+    //   .single();
+    //
+    // if (profile?. role === "ADMIN") {
+    //   router.replace("/(admin)");
+    // } else {
+    //   router.replace("/(worker)");
+    // }
+    
+    await new Promise(resolve => setTimeout(resolve, 3000));
+    router.replace("/(auth)/login");
+    
+  };
+
+  return (
+    <View style={styles.container}>
+      <ActivityIndicator size="large" color="#3B82F6" />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F9FAFB",
+  },
+});
