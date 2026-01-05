d32b7bccb5c26bab97ceb0b447f60987ffe36d26
Jhonatan-calle <Jhonatancallegaleano@gmail.com>
2026-01-04 21:11:31 -0300

    add auth to app index

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,20 @@
-import { Text, View } from "react-native";
+import { useState, useEffect } from 'react'
+import { supabase } from './lib/supabase'
+import Auth from './components/Auth'
+import { View } from 'react-native'
+import { Session } from '@supabase/supabase-js'
 
-export default function Index() {
+export default function App() {
+  const [session, setSession] = useState<Session | null>(null)
 
-  return (
-    <View>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
+  // mostrar login o iniciar secions
 }
