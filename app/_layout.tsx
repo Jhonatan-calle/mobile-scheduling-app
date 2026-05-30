a2da4665feec7121d61259e21891b39331e7bc99
Jhonatan-calle <Jhonatancallegaleano@gmail.com>
2026-05-30 01:52:37 -0300

    add color to status bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,11 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 
+
 export default function RootLayout() {
   return (
     <>
+      <StatusBar style="dark" backgroundColor="#F9FAFB" />
       <Stack
         screenOptions={{
           headerShown: false,
